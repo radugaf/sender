6fb628b42c5ad77bef5cb86753bdc91e28508f87
Radu Gafita <radu.gafita@tcnbroadcasting.com>
2023-06-19 13:09:18 +0300

    Added csrf

--- sender/config/settings/local.py
+++ sender/config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "api.albsender.xyz"]
-
+CSRF_TRUSTED_ORIGINS = ["api.albsender.xyz"]
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
