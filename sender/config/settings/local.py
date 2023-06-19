e9ed24328502fddcafa61ecd34e4d378408cef96
Radu Gafita <radu.gafita@tcnbroadcasting.com>
2023-06-19 13:14:47 +0300

    updatede

--- sender/config/settings/local.py
+++ sender/config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "api.albsender.xyz"]
-CSRF_TRUSTED_ORIGINS = ["https://api.albsender.xyz"]
+CSRF_TRUSTED_ORIGINS = ["https://api.albsender.xyz", "https://api.albsender.xyz/api/v1/user/sender/"]
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
