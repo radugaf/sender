23e513eca4a9d01075541f577694bcb3d9120d0f
Radu Gafita <radu.gafita@tcnbroadcasting.com>
2023-06-19 13:17:42 +0300

    updated again

--- sender/config/settings/base.py
+++ sender/config/settings/base.py
@@ -313,7 +313,8 @@ SOCIALACCOUNT_FORMS = {"signup": "sender.users.forms.UserSocialSignupForm"}
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
+        'rest_framework.authentication.BasicAuthentication',
         "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
